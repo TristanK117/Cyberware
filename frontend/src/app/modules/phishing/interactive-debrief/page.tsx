9780860783b34aa97935a110f356f69ea152857b
justmai1 <justmai@uw.edu>
2026-04-20 14:09:13 -0700

    changes to debriefing screen info

--- frontend/src/app/modules/phishing/interactive-debrief/page.tsx
+++ frontend/src/app/modules/phishing/interactive-debrief/page.tsx
@@ -1,5 +1,15 @@
+"use client";
+import { useState } from "react";
+import { PhishingEmailMockup, ModuleHeader } from "../context/page";
+import phishingModule from "../../../../../module_details/phishing_scams.json";
+import "../../modules.css";
+import Link from "next/link"
+
 export default function InteractiveDebrief(){
     return(
-        <h1>This is the interactive debriefing section</h1>
+        <div>
+            <ModuleHeader/>
+            <PhishingEmailMockup/>
+        </div>
     )
 }
